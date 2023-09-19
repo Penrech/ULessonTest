eef6aefc31c9036966ec1dc9c750ccd5b76c1ead
Pau Enrech <pauenrech94@gmail.com>
2023-09-19 21:31:24 +0200

    add drawables and initial theme config

---
+++ common-resources/src/main/java/com/enrech/ulessontest/common_resources/extensions/ColorExtensions.kt
@@ -0,0 +1,5 @@
+package com.enrech.ulessontest.common_resources.extensions
+
+import androidx.compose.ui.graphics.Color
+
+fun Color.withAlpha(alpha: Float) = this.copy(alpha = alpha)
---
+++ common-resources/src/main/java/com/enrech/ulessontest/common_resources/theme/Shape.kt
@@ -0,0 +1,2 @@
+package com.enrech.ulessontest.common_resources.theme
+
