55fac05c4f0766835c74e23bdf0a1df4688be01a
Pau Enrech <pauenrech94@gmail.com>
2023-09-19 20:01:49 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://repo.repsy.io/mvn/chrynan/public") }
+    }
+    dependencies {
+        classpath(libs.gradle.plugin)
+        classpath(libs.kotlin.plugin)
+        classpath(libs.androidx.nav.safe.args.plugin)
+        classpath(libs.dagger.plugin)
+        classpath(libs.kotlin.dokka)
+        classpath(libs.kotlin.serialization.plugin)
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://repo.repsy.io/mvn/chrynan/public") }
+    }
+}
+
+tasks.register<Delete>("clean").configure {
+    delete(rootProject.buildDir)
+}
