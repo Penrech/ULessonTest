e50be718016cff45442a2d8311d5c5b7ae0b734b
Pau Enrech <pauenrech94@gmail.com>
2023-09-20 15:43:01 +0200

    add all fonts

--- common-resources/src/main/java/com/enrech/ulessontest/common_resources/theme/Fonts.kt
+++ common-resources/src/main/java/com/enrech/ulessontest/common_resources/theme/Fonts.kt
@@ -1,2 +1,49 @@
 package com.enrech.ulessontest.common_resources.theme
 
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.enrech.ulessontest.common_resources.R
+
+val mplusFontFamily = FontFamily(
+    Font(R.font.mplusrounded1c_black, weight = FontWeight.Black),
+    Font(R.font.mplusrounded1c_bold, weight = FontWeight.Bold),
+    Font(R.font.mplusrounded1c_extrabold, weight = FontWeight.ExtraBold),
+    Font(R.font.mplusrounded1c_light, weight = FontWeight.Light),
+    Font(R.font.mplusrounded1c_medium, weight = FontWeight.Medium),
+    Font(R.font.mplusrounded1c_regular, weight = FontWeight.Normal),
+    Font(R.font.mplusrounded1c_thin, weight = FontWeight.Thin),
+)
+
+val muliFontFamily = FontFamily(
+    Font(R.font.muli),
+    Font(R.font.muli_bold, weight = FontWeight.Bold),
+    Font(R.font.muli_bolditalic, weight = FontWeight.Bold, style = FontStyle.Italic),
+    Font(R.font.muli_extralight, weight = FontWeight.ExtraLight),
+    Font(R.font.muli_extralightitalic, weight = FontWeight.ExtraLight, style = FontStyle.Italic),
+    Font(R.font.muli_italic, style = FontStyle.Italic),
+    Font(R.font.muli_light, weight = FontWeight.Light),
+    Font(R.font.muli_lightitalic, weight = FontWeight.Light, style = FontStyle.Italic),
+    Font(R.font.muli_semibold, weight = FontWeight.SemiBold),
+    Font(R.font.muli_semi_bolditalic, weight = FontWeight.SemiBold, style = FontStyle.Italic),
+)
+
+val mulishFontFamily = FontFamily(
+    Font(R.font.mulish_black, FontWeight.Black),
+    Font(R.font.mulish_blackitalic, FontWeight.Black, FontStyle.Italic),
+    Font(R.font.mulish_bold, FontWeight.Bold),
+    Font(R.font.mulish_bolditalic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.mulish_extrabold, FontWeight.ExtraBold),
+    Font(R.font.mulish_extrabolditalic, FontWeight.ExtraBold, FontStyle.Italic),
+    Font(R.font.mulish_extralight, FontWeight.ExtraLight),
+    Font(R.font.mulish_extralightitalic, FontWeight.ExtraLight, FontStyle.Italic),
+    Font(R.font.mulish_italic, style = FontStyle.Italic),
+    Font(R.font.muli_light, FontWeight.Light),
+    Font(R.font.muli_lightitalic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.mulish_medium, FontWeight.Medium),
+    Font(R.font.mulish_mediumitalic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.mulish_regular),
+    Font(R.font.mulish_semibold, FontWeight.SemiBold),
+    Font(R.font.mulish_semibolditalic, FontWeight.SemiBold, FontStyle.Italic),
+)
